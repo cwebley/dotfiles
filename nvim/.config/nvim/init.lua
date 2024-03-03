0b99a5a5ebd09e1365d38cb091fed8b2b0122053
cwebley <cameron.webley@gmail.com>
2024-03-03 13:24:32 -0600

    feat(nvim): adds a few things from kickstart

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,9 +5,50 @@ vim.cmd("set expandtab")
 vim.cmd("set relativenumber")
 vim.cmd("set number")
 
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+-- vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Sets how neovim will display certain whitespace in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+-- vim.opt.list = true
+-- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>E', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -34,6 +75,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
 -- Paste without overwriting the register
 vim.keymap.set("v", "p", '"_dP')
+
 -- highlight yanked region. maybe this belongs in an autocmd file as more get added
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,22 +1,86 @@
 return {
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({})
-    end,
-  },
-  {
-    "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup()
-    end,
-  },
-  {
-    "numToStr/Comment.nvim",
-    config = function()
-      require("Comment").setup({})
-    end,
-  },
+	{
+		"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup()
+		end,
+	},
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+	},
+
+	-- TODO this seems great, haven't tried
+	-- { -- Autoformat
+	--    'stevearc/conform.nvim',
+	--    opts = {
+	--      notify_on_error = false,
+	--      format_on_save = {
+	--        timeout_ms = 500,
+	--        lsp_fallback = true,
+	--      },
+	--      formatters_by_ft = {
+	--        lua = { 'stylua' },
+	--        -- Conform can also run multiple formatters sequentially
+	--        -- python = { "isort", "black" },
+	--        --
+	--        -- You can use a sub-list to tell conform to run *until* a formatter
+	--        -- is found.
+	--        -- javascript = { { "prettierd", "prettier" } },
+	--      },
+	--    },
+	-- },
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+	{ -- Collection of various small independent plugins/modules
+		"echasnovski/mini.nvim",
+		config = function()
+			-- Better Around/Inside textobjects
+			--
+			-- Examples:
+			--  - va)  - [V]isually select [A]round [)]paren
+			--  - yinq - [Y]ank [I]nside [N]ext [']quote
+			--  - ci'  - [C]hange [I]nside [']quote
+			require("mini.ai").setup({ n_lines = 500 })
+
+			-- Add/delete/replace surroundings (brackets, quotes, etc.)
+			--
+			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+			-- - sd'   - [S]urround [D]elete [']quotes
+			-- - sr)'  - [S]urround [R]eplace [)] [']
+			require("mini.surround").setup()
+
+			-- Simple and easy statusline.
+			--  You could remove this setup call if you don't like it,
+			--  and try some other statusline plugin
+			local statusline = require("mini.statusline")
+			statusline.setup()
+
+			-- You can configure sections in the statusline by overriding their
+			-- default behavior. For example, here we disable the section for
+			-- cursor information because line numbers are already enabled
+			---@diagnostic disable-next-line: duplicate-set-field
+			statusline.section_location = function()
+				return ""
+			end
+
+			-- ... and there is more!
+			--  Check out: https://github.com/echasnovski/mini.nvim
+		end,
+	},
 }
