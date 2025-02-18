65b72cb5070bce081cb40b4bb80ff1f2a0e047b7
Cameron Webley <cameronwebley@gmail.com>
2025-02-17 23:35:42 -0600

    feat(nvim): adds copilot to lazyvim, and lazygit

---
+++ nvim/.config/nvim/lua/plugins/extend-blink.lua
@@ -0,0 +1,10 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      -- lazyvim sets this to "Enter"
+      -- and the CR accept funcationlity is frustrating
+      preset = "default",
+    },
+  },
+}
